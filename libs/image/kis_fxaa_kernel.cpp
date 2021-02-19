57e158ba0af03f4bf0d80f13d556c5b3e80169c7
Charlton Rodda <charlton.rodda@gmail.com>
2021-02-19 22:39:06 +0000

    WIP WIP

--- libs/image/kis_fxaa_kernel.cpp
+++ libs/image/kis_fxaa_kernel.cpp
@@ -25,6 +25,15 @@
 #include <KoColorSpace.h>
 #include <kis_iterator_ng.h>
 #include <QVector3D>
+#include <kis_fixed_paint_device.h>
+#include <KisSequentialIteratorProgress.h>
+
+
+struct pixelEdgeFlags
+{
+    bool edgeAtRight;
+    bool edgeAtBottom;
+};
 
 KisFXAAKernel::KisFXAAKernel()
 {
@@ -41,4 +50,62 @@ void KisFXAAKernel::applyFXAA(KisPaintDeviceSP device,
                               int searchRadius)
 {
 
+    const KoColorSpace* cs = device->colorSpace();
+
+    KisFixedPaintDevice luma(cs);
+    luma.setRect(rect);
+    luma.lazyGrowBufferWithoutInitialization();
+    device->readBytes(luma.data(), luma.bounds());
+
+    qInfo() << "applying AA filter";
+    calculateLuma(cs, luma.data(), luma.data(), luma.allocatedPixels());
+
+    // DEBUG: write luma to the luma
+    // dst->writeBytes(luma.data(), luma.bounds());
+
+    luma.convertTo(KoColorSpaceRegistry::instance()->graya16("Gray-D50-elle-V4-srgbtrc.icc"));
+
+    QVector<pixelEdgeFlags> edgeFlagRowInit = QVector<pixelEdgeFlags>(luma.bounds().width(), pixelEdgeFlags{false,false});
+    QVector<QVector<pixelEdgeFlags>> edgeFlags(luma.bounds().height(), edgeFlagRowInit);
+
+    // KisSequentialConstIteratorProgress lumaIt(luma, rect);
+    // while (lumaIt.nextPixel()) {
+    //     calculateEdgeFlags(luma.colorSpace(), lumaIt.rawData(), edgeFlags, lumaIt.x(), lumaIt.y());
+    // }
+
+    const int tileHeightMinus1 = rect.height() - 1;
+    const int tileWidthMinus1 = rect.width() - 1;
+
+    // Foreach INNER pixel in tile
+
+    QRect rectTopLeft = rectTopLeft.adjusted(0, 0, -1, -1);
+    QRect rectTopRight = rectTopLeft.translated(1, 0);
+    QRect rectBottomLeft = rectTopLeft.translated(0, 1);
+    KisSequentialConstIteratorProgress topLeftIt(luma, rectTopLeft);
+    KisSequentialConstIteratorProgress topRightIt(luma, rectTopRight);
+    KisSequentialConstIteratorProgress bottomLeftIt(luma, rectBottomLeft);
+    while 
+            double pos = y * rect.width() + x;
+            double posdown = (y - 1) * rect.width() + x;
+            double posright = y * rect.width() + x + 1;
+
+            bool edgeAtRight = abs(luma[pos] - luma[posright]) > FXAA_THRESHOLD;
+            bool edgeAtBottom = abs(luma[pos] - luma[posdown]) > FXAA_THRESHOLD;
+
+            edgeFlags[x][y] = pixelEdgeFlags{edgeAtRight, edgeAtBottom};
+        }
+    }
 }
+
+void calculateLuma(const KoColorSpace* cs, quint8* src, quint8* dst, qint32 nPixels) {
+    QHash<QString, QVariant> params;
+    params["type"] = 2; // BT601
+    KoColorTransformation* transfo;
+    transfo = cs->createColorTransformation("desaturate_adjustment", params);
+    transfo->transform(src, dst, nPixels);
+}
+
+// inline void calculateEdgeFlags(const KoColorSpace* cs, quint8* luma, QVector<QVector<pixelEdgeFlags>>& edgeFlags, int x, int y) {
+// {
+
+// }
